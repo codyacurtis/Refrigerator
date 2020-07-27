12bd75df75e7d41d626d3680c31dfe08596e7883
Cody C <cody.a.curtis@gmail.com>
2020-07-27 13:52:22 -0500

    initial gui stuff

--- src/GUIDisplay.java
+++ src/GUIDisplay.java
@@ -1,4 +1,12 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUIDisplay {
+public class GUIDisplay implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
