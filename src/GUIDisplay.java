7ae7dec0341aa583bfca11e72271fd12e159042c
Cody C. <cody.a.curtis@gmail.com>
2020-08-02 19:05:01 -0500

    closing window exits program

--- src/GUIDisplay.java
+++ src/GUIDisplay.java
@@ -5,6 +5,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -44,6 +46,13 @@ public class GUIDisplay extends JFrame implements ActionListener, RefrigeratorDi
 	public GUIDisplay() {
 		super("Refrigerator");
 		getContentPane().setLayout(new GridBagLayout());
+
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent event) {
+				System.exit(0);
+			}
+		});
 
 		JPanel thePanel = new JPanel();
 		thePanel.setLayout(new GridBagLayout());
